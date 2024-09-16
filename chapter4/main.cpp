d094e30fe7f7d4b777a9c2cd67dabd7a289e943d
Tony Nordstrom <95768015+tnordstrom@users.noreply.github.com>
2024-09-16 16:20:46 -0700

    Chapter 4 exercises

---
+++ chapter4/main.cpp
@@ -0,0 +1,54 @@
+/* Tony Nordstrom - September 16 02024 */
+#include <iostream>
+
+int enterNumber(void)
+{
+	int num{ };
+	std::cout << "Enter a number 10 or lower: ";
+	std::cin >> num;
+	if (num > 10)
+	{
+		std::cout << "Out of range\n";
+	}
+	return(num);
+}
+
+/* Determines if a number <= 10 is prime, greater numbers always return false */
+bool isPrime(int val)
+{
+	switch(val)
+	{
+		case 0:
+		case 1:
+		case 4:
+		case 6:
+		case 8:
+		case 9:
+		case 10:
+			return false;
+			break;
+		case 2:
+		case 3:
+		case 5:
+		case 7:
+			return true;
+			break;
+		default:
+			return false;
+			break;
+	}
+	return false;
+}
+
+int main()
+{
+	char ch{ 'a' };
+	std::cout << ch << " is equivalent to " << static_cast<int>(ch) << '\n';
+	
+	int val{ enterNumber() };
+	
+	std::cout << val << " is " << (isPrime(val) ? "prime" : "not prime") << '\n';
+	
+	
+	return 0;
+}
