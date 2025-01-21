4f1666af92d0d7b1698bbe41320394ed4e32987d
Tony Nordstrom <95768015+tnordstrom@users.noreply.github.com>
2025-01-20 21:08:04 -0800

    Chapter 13.10 Quiz 2 - First cut

---
+++ Chapter13.10quiz2/main.cpp
@@ -0,0 +1,18 @@
+/* Tony Nordstrom */
+/* January 15 02025 */
+/* Chapter 13.10 quiz question 2 */
+
+#include <iostream>
+
+struct Fraction
+{
+	int numerator { 0 };
+	int denominator { 1 };
+};
+	
+
+int main ( void )
+{
+	
+	return 0;
+}
