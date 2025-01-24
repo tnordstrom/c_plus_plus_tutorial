567e76628521890cd37ceb49a59cd06daebabe77
Tony Nordstrom <95768015+tnordstrom@users.noreply.github.com>
2025-01-24 08:48:05 -0800

    Finished Chapter13.10quiz2 and 3

--- Chapter13.10quiz2/main.cpp
+++ Chapter13.10quiz2/main.cpp
@@ -1,6 +1,7 @@
 /* Tony Nordstrom */
 /* January 15 02025 */
 /* Chapter 13.10 quiz question 2 */
+/* question 3: because the value must hang around outside scope of getFraction? */
 
 #include <iostream>
 
@@ -10,9 +11,47 @@ struct Fraction
 	int denominator { 1 };
 };
 	
+Fraction getFraction(void)
+{
+	Fraction temp;
+	
+	std::cout << "Enter a value for the numerator: ";
+	std::cin >> temp.numerator;
+	std::cout << "Enter a value for the denominator: ";
+	std::cin >> temp.denominator;
+	
+	return (temp);
+}
+
+constexpr Fraction multFraction(const Fraction& a, const Fraction& b)
+{
+	Fraction result;
+	
+	result.numerator = a.numerator * b.numerator;
+	result.denominator = a.denominator * b.denominator;
+	
+	return (result);
+}
+
+void printFraction(const Fraction& f)
+{
+	std::cout << f.numerator << "/" << f.denominator;
+	
+	return;
+}
 
 int main ( void )
 {
+	Fraction a;
+	Fraction b;
+	Fraction c;
+	
+	a = getFraction();
+	b = getFraction();
+	c = multFraction(a, b);
+	std::cout << "Your fractions multiplied together: ";
+	printFraction(c);
+	std::cout << '\n';
 	
 	return 0;
 }
